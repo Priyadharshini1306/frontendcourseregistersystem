ec86f98f484840ad934bf9cf4da84d8a1dc1f799
priyadharshini R <priyadharshinir.ad24@bitsathy.ac.in>
2025-08-18 14:55:50 +0530

    first commit

---
+++ myscript.js
@@ -0,0 +1,37 @@
+function showCourses()
+{
+    alert("hi your are");
+    fetch("http://localhost:8080/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+        const dataTable = document.getElementById("courseTable")
+        courses.forEach(course => {
+            var row = `<tr>
+            <td>${course.courseId}</td>
+            <td>${course.couseName}</td>
+            <td>${course.trainer}</td>
+            <td>${course.durationInWeeks}</td>
+            </tr>`
+            dataTable.innerHTML+=row;
+        });
+
+    });
+}
+function showEnrolledStudents()
+{
+    alert("hi your are");
+    fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+        const dataTable = document.getElementById("enrolledTable")
+        students.forEach(student => {
+            var row = `<tr>
+            <td>${student.name}</td>
+            <td>${student.emailId}</td>
+            <td>${student.courseName}</td>
+            </tr>`
+            dataTable.innerHTML+=row;
+        });
+
+    });
+}
